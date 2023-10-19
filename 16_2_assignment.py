aace7511a9875efb8b58b90c67cf13be1cf1c122
moejasim <mjjasim207@gmail.com>
2023-10-18 23:29:04 -0500

    16_2_2_assignment.ph.py , 16_2_assignment.py

---
+++ 16_2_assignment.py
@@ -0,0 +1,8 @@
+def show_employee(name, salary):
+    print(f"Employee Name: {name}, Salary- ${salary}")
+
+show_employee(name="Moe jasim", salary=60000)
+
+show_employee("John Smith", 70000)
+print("Employee name:  Bob Smith,Salary- $55000")
+
